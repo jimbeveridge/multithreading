010a9db054ee807b04421a1b76825557ae03e557
Jim Beveridge <jimb@connectedsw.com>
2020-05-04 15:32:54 -0700

    Removed obsolete DEBUG_NEW code

--- src/Cancel.11/CancView.cpp
+++ src/Cancel.11/CancView.cpp
@@ -17,8 +17,6 @@
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
-#undef THIS_FILE
-static char THIS_FILE[] = __FILE__;
 #endif
 
 DWORD WINAPI ThreadFunc(LPVOID);
@@ -102,13 +100,13 @@ void CCancelView::OnViewTightLoop()
 
 void CCancelView::OnLaunch() 
 {
-	AfxMessageBox("About to launch the progress dialog"
-		" in another thread using CWinThread.  The program will appear"
-		" to hang while a busy loop in the main thread runs.  The dialog"
-		" will not appear until the busy loop finishes."
-		"\n\nOnce the dialog is up, the slider control works.  However, if you"
-		" select View/Tight Loop, the slider control stops working."
-		"\n\nChoose View/Destroy to close the dialog.");
+	AfxMessageBox(_T("About to launch the progress dialog")
+		_T(" in another thread using CWinThread.  The program will appear")
+		_T(" to hang while a busy loop in the main thread runs.  The dialog")
+		_T(" will not appear until the busy loop finishes.")
+		_T("\n\nOnce the dialog is up, the slider control works.  However, if you")
+		_T(" select View/Tight Loop, the slider control stops working.")
+		_T("\n\nChoose View/Destroy to close the dialog."));
 
 	gCanDlg.StartDialog();
 	for (int i=0;i<200000000; i++);
@@ -124,25 +122,25 @@ void CCancelView::OnBoom()
 //
 void CCancelView::OnLaunchWin32() 
 {
-	AfxMessageBox("About to launch the progress dialog"
-		" in another thread using CreateThread().  The dialog's parent is the mainframe."
-		"\n\nThe program will appear"
-		" to hang while a busy loop in the main thread runs.  The dialog"
-		" will not appear until the busy loop finishes."
-		"\n\nChoose File/Exit to close the dialog.");
+	AfxMessageBox(_T("About to launch the progress dialog")
+		_T(" in another thread using CreateThread().  The dialog's parent is the mainframe.")
+		_T("\n\nThe program will appear")
+		_T(" to hang while a busy loop in the main thread runs.  The dialog")
+		_T(" will not appear until the busy loop finishes.")
+		_T("\n\nChoose File/Exit to close the dialog."));
 
 	LaunchHelper( AfxGetMainWnd()->GetSafeHwnd() );
 }
 
 void CCancelView::OnViewNoparent() 
 {
-	AfxMessageBox("About to launch the progress dialog"
-		" in another thread using CreateThread().  The dialog has no parent."
-		" Notice that minimizing and the taskbar will not set be setup properly."
-		"\n\nThe PARENT WINDOW will appear"
-		" to hang while a busy loop in the main thread runs, but the dialog"
-		" will appear immediately and be fully functional."
-		"\n\nChoose File/Exit to close the dialog.");
+	AfxMessageBox(_T("About to launch the progress dialog")
+		_T(" in another thread using CreateThread().  The dialog has no parent.")
+		_T(" Notice that minimizing and the taskbar will not set be setup properly.")
+		_T("\n\nThe PARENT WINDOW will appear")
+		_T(" to hang while a busy loop in the main thread runs, but the dialog")
+		_T(" will appear immediately and be fully functional.")
+		_T("\n\nChoose File/Exit to close the dialog."));
 
 	LaunchHelper( NULL );
 }
--- src/Cancel.11/Cancel.cpp
+++ src/Cancel.11/Cancel.cpp
@@ -16,8 +16,6 @@
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
-#undef THIS_FILE
-static char THIS_FILE[] = __FILE__;
 #endif
 
 /////////////////////////////////////////////////////////////////////////////
@@ -57,12 +55,6 @@ BOOL CCancelApp::InitInstance()
 	// If you are not using these features and wish to reduce the size
 	//  of your final executable, you should remove from the following
 	//  the specific initialization routines you do not need.
-
-#ifdef _AFXDLL
-	Enable3dControls();			// Call this when using MFC in a shared DLL
-#else
-	Enable3dControlsStatic();	// Call this when linking to MFC statically
-#endif
 
 	LoadStdProfileSettings();  // Load standard INI file options (including MRU)
 
