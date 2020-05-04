251d5b9d80059af06435452cf6e71f5a8066522a
Jim Beveridge <jimb@connectedsw.com>
2020-05-04 15:24:00 -0700

    Updated MtVerify.h for Unicode

--- src/MtVerify.h
+++ src/MtVerify.h
@@ -2,7 +2,7 @@
  * MtVerify.h
  *
  * Error handling for applications in
- * "Multitheading Applications in Win32"
+ * "Multithreading Applications in Win32"
  *
  * The function PrintError() is marked as __inline so that it can be
  * included from one or more C or C++ files without multiple definition
@@ -12,20 +12,21 @@
  * so the function will be globally available.
  */
 
-#pragma comment( lib, "USER32" )
+#pragma once
 
 #include <crtdbg.h>
+#include <tchar.h>
 #define MTASSERT(a) _ASSERTE(a)
 
 
-#define MTVERIFY(a) if (!(a)) PrintError(#a,__FILE__,__LINE__,GetLastError())
+#define MTVERIFY(a) if (!(a)) PrintError(#a, __FILE__, __LINE__, GetLastError())
 
 __inline void PrintError(LPSTR linedesc, LPSTR filename, int lineno, DWORD errnum)
 {
-	LPSTR lpBuffer;
-	char errbuf[256];
+	LPTSTR lpBuffer = 0;
+	TCHAR errbuf[256];
 #ifdef _WINDOWS
-	char modulename[MAX_PATH];
+	TCHAR modulename[MAX_PATH];
 #else // _WINDOWS
 	DWORD numread;
 #endif // _WINDOWS
@@ -34,19 +35,20 @@ __inline void PrintError(LPSTR linedesc, LPSTR filename, int lineno, DWORD errnu
 			| FORMAT_MESSAGE_FROM_SYSTEM,
 		NULL,
 		errnum,
-		LANG_NEUTRAL,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
 		(LPTSTR)&lpBuffer,
 		0,
 		NULL );
 
-	wsprintf(errbuf, "\nThe following call failed at line %d in %s:\n\n"
-               "    %s\n\nReason: %s\n", lineno, filename, linedesc, lpBuffer);
+	_sntprintf_s(errbuf, sizeof(errbuf), _TRUNCATE, _T("\nThe following call failed at line %d in %hs:\n\n")
+               _T("    %hs\n\nReason: %s\n"), lineno, filename, linedesc, lpBuffer);
 #ifndef _WINDOWS
-	WriteFile(GetStdHandle(STD_ERROR_HANDLE), errbuf, strlen(errbuf), &numread, FALSE );
+	WriteFile(GetStdHandle(STD_ERROR_HANDLE), errbuf, _tcslen(errbuf), &numread, FALSE );
 	Sleep(3000);
 #else
 	GetModuleFileName(NULL, modulename, MAX_PATH);
 	MessageBox(NULL, errbuf, modulename, MB_ICONWARNING|MB_OK|MB_TASKMODAL|MB_SETFOREGROUND);
 #endif
+	LocalFree(lpBuffer);
 	exit(EXIT_FAILURE);
 }
